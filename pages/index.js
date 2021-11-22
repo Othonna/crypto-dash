b67eac11142e355006e5fc0d0f21189e94586065
Janick Othonna <janick.deneux@gmail.com>
2021-11-22 23:35:58 +0100

    container & card

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import Head from 'next/head'
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
+import Container from './components/Container';
 
 
 export default function Home() {
   return (
   <div>
-    <div>
+    <div className='flex '>
       <Sidebar />
+      <div className='w-screen'>
       <Header />
+      <Container />
+      </div>
     </div>
   </div>
   )
